4f9ce4816c74721c97b12e592443889050043177
c密碼 <h35765469@yahoo.com.tw>
2018-04-28 02:03:35 +0800

    添加收起鍵盤的功能

--- app/src/main/java/com/example/john/mimicvideo/BaseActivity.java
+++ app/src/main/java/com/example/john/mimicvideo/BaseActivity.java
@@ -1,5 +1,7 @@
 package com.example.john.mimicvideo;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
@@ -8,6 +10,8 @@ import android.os.Message;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import com.facebook.CallbackManager;
 
@@ -22,5 +26,12 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
+    }
+
+    //hide the keyboard
+    public static void hideSoftKeyboard (Activity activity, View view)
+    {
+        InputMethodManager imm = (InputMethodManager)activity.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(view.getApplicationWindowToken(), 0);
     }
 }
