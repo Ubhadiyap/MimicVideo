267996ebfc260ccbc93aa1801633b5d723d39971
c密碼 <h35765469@yahoo.com.tw>
2018-04-28 23:40:14 +0800

    添加equals function

--- app/src/main/java/com/example/john/mimicvideo/model/Like.java
+++ app/src/main/java/com/example/john/mimicvideo/model/Like.java
@@ -11,4 +11,15 @@ public class Like implements Serializable {
     public int user_id;
     public int video_content_id;
     public int is_click;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Like))
+            return false;
+        Like other = (Like) obj;
+        System.out.println("wire " + (user_id != 0 && user_id == other.user_id));
+        return user_id != 0 && user_id == other.user_id;//Compare Id if null falseF
+    }
 }
