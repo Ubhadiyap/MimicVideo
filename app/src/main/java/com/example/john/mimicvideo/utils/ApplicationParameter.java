72c72e0a9c821aeba2469a9c31a32d3b3f59fb98
c密碼 <h35765469@yahoo.com.tw>
2018-04-28 22:32:12 +0800

    全域的參數

---
+++ app/src/main/java/com/example/john/mimicvideo/utils/ApplicationParameter.java
@@ -0,0 +1,13 @@
+package com.example.john.mimicvideo.utils;
+
+/**
+ * Created by john on 2018/4/27.
+ */
+
+public class ApplicationParameter {
+    public static int BACK_SHOW_VIDEO_CONTENT = 2;
+
+    //below for loading activity go .
+    public static int TO_RECORD = 0;
+    public static int TO_SHARE = 1;
+}
