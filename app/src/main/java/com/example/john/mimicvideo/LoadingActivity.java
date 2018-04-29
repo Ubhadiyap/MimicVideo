8a209368dab308ed5f2b17ebb836d50ad12b2ffc
c密碼 <h35765469@yahoo.com.tw>
2018-04-29 22:06:53 +0800

    添加發布的loading

--- app/src/main/java/com/example/john/mimicvideo/LoadingActivity.java
+++ app/src/main/java/com/example/john/mimicvideo/LoadingActivity.java
@@ -1,9 +1,16 @@
 package com.example.john.mimicvideo;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.androidnetworking.AndroidNetworking;
@@ -12,11 +19,39 @@ import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.DownloadListener;
 import com.androidnetworking.interfaces.DownloadProgressListener;
 import com.daimajia.numberprogressbar.NumberProgressBar;
+import com.example.john.mimicvideo.api.Api;
 import com.example.john.mimicvideo.utils.ApplicationParameter;
+import com.example.john.mimicvideo.utils.JSONParser;
+import com.example.john.mimicvideo.utils.SharePreferenceDB;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class LoadingActivity extends BaseActivity {
     NumberProgressBar numberProgressBar;
+    private TextView loadingTitleTxt;
     private int to;
+    private SharePreferenceDB sharePreferenceDB;
+    private JSONParser jsonParser = new JSONParser();
+
+    int allprogress = 0;
+
+    @Override
+    public void onBackPressed() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +59,19 @@ public class LoadingActivity extends BaseActivity {
         setContentView(R.layout.activity_loading);
         to = getIntent().getIntExtra("to", 0);
         numberProgressBar = findViewById(R.id.number_progress_bar);
-        if(getIntent().getIntExtra("video_sample_id", 0) != 0){
-            int videoSampleId = getIntent().getIntExtra("video_sample_id", 0);
+        loadingTitleTxt = findViewById(R.id.loadingTitleTxt);
+
+        sharePreferenceDB = new SharePreferenceDB(this);
+        if(getIntent().getStringExtra("video_sample_url") != null){
+            int videoSampleId = getIntent().getIntExtra("videoSampleId", 0);
             String videoSampleUrl = getIntent().getStringExtra("video_sample_url");
-            downLoadFileFromUrl(videoSampleUrl, "/storage/emulated/0/", "share_content.mp4", videoSampleId);
+            downLoadFileFromUrl(videoSampleUrl, Environment
+                    .getExternalStorageDirectory().toString(), "share_content.mp4", videoSampleId);
         }else if(getIntent().getStringExtra("videoContentUrl") != null){
             int videoSampleId = 0;
             String videoContentUrl = getIntent().getStringExtra("videoContentUrl");
-            downLoadFileFromUrl(videoContentUrl, "/storage/emulated/0/", "share_content.mp4", 0);
+            downLoadFileFromUrl(videoContentUrl, Environment
+                    .getExternalStorageDirectory().toString(), "share_content.mp4", 0);
         }else if(getIntent().getStringExtra("cameraVideoUrl") != null){
             int videoSampleId = 0;
             String cameraVideoUrl = getIntent().getStringExtra("cameraVideoUrl");
@@ -40,10 +80,19 @@ public class LoadingActivity extends BaseActivity {
             intent.putExtra("videoSampleId", videoSampleId);
             intent.putExtra("videoUrl", cameraVideoUrl);
             startActivity(intent);
+        }else if(getIntent().getStringExtra("videoContentTitle") !=null){
+            loadingTitleTxt.setText("發布中");
+            int user_id = sharePreferenceDB.getInt("id");
+            int videoSampleId = getIntent().getIntExtra("videoSampleId", 0);
+            String videoContentTitle = getIntent().getStringExtra("videoContentTitle");
+            File file = new File(ApplicationParameter.FILE_SAVE_PATH);
+            String fileName = UUID.randomUUID().toString() + "_" + UUID.randomUUID() + "_" + System.currentTimeMillis() + ".mp4";
+            new PostVideoContent(user_id, videoSampleId, videoContentTitle, fileName).execute();
+            new BackgroundUploader(LoadingActivity.this, Api.baseUrl + "upload_video_content_file.php", file, fileName).execute();
         }
     }
 
-    public void downLoadFileFromUrl(String url, String dirPath, String fileName, int videoSampleId){
+    public void downLoadFileFromUrl(String url, String dirPath, String fileName, final int videoSampleId){
         AndroidNetworking.download(url,dirPath,fileName)
                 .setTag("downloadTest")
                 .setPriority(Priority.MEDIUM)
@@ -62,6 +111,7 @@ public class LoadingActivity extends BaseActivity {
                         Intent intent = new Intent();
                         if(to == ApplicationParameter.TO_RECORD){
                             intent.setClass(LoadingActivity.this, TestVideoActivity.class);
+                            intent.putExtra("videoSampleId", videoSampleId);
                         }else{
                             intent.setClass(LoadingActivity.this, ShareActivity.class);
                         }
@@ -76,7 +126,183 @@ public class LoadingActivity extends BaseActivity {
                 });
     }
 
-    @Override
-    public void onBackPressed() {
+    class PostVideoContent extends AsyncTask<String, String, String> {
+        int user_id;
+        int video_sample_id;
+        String title;
+        String path;
+
+        PostVideoContent(int user_id, int video_sample_id, String title, String path){
+            this.user_id = user_id;
+            this.video_sample_id = video_sample_id;
+            this.title = title;
+            this.path = path;
+        }
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        /**
+         * Creating product
+         * */
+        protected String doInBackground(String... args) {
+//            String name = inputName.getText().toString();
+//            String price = inputPrice.getText().toString();
+//            String description = inputDesc.getText().toString();
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("user_id", String.valueOf(user_id)));
+            params.add(new BasicNameValuePair("video_sample_id", String.valueOf(video_sample_id)));
+            params.add(new BasicNameValuePair("title", title));
+            params.add(new BasicNameValuePair("path", path));
+
+            // getting JSON Object
+            // Note that create product url accepts POST method
+            JSONObject jsonObject = jsonParser.makeHttpRequest(Api.baseUrl + "create_video_content.php",
+                    "POST", params);
+
+            // check log cat fro response
+            Log.d("Create Response", jsonObject.toString());
+
+//            // check for success tag
+//            try {
+//                int success = json.getInt(TAG_SUCCESS);
+//
+//                if (success == 1) {
+//                    // successfully created product
+//                    Intent i = new Intent(getApplicationContext(), AllProductsActivity.class);
+//                    startActivity(i);
+//
+//                    // closing this screen
+//                    finish();
+//                } else {
+//                    // failed to create product
+//                }
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+        }
+
+    }
+
+    class BackgroundUploader extends AsyncTask<Void, Integer, Void> implements DialogInterface.OnCancelListener {
+        private Context context;
+        private String url;
+        private File file;
+        private String fileName;
+
+        public BackgroundUploader(Context context, String url, File file, String fileName) {
+            this.context = context;
+            this.url = url;
+            this.file = file;
+            this.fileName = fileName;
+        }
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected Void doInBackground(Void... v) {
+            HttpURLConnection.setFollowRedirects(false);
+            HttpURLConnection connection = null;
+            try {
+                connection = (HttpURLConnection) new URL(url).openConnection();
+                connection.setRequestMethod("POST");
+                String boundary = "---------------------------boundary";
+                String tail = "\r\n--" + boundary + "--\r\n";
+                connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+                connection.setDoOutput(true);
+
+                String metadataPart = "--" + boundary + "\r\n"
+                        + "Content-Disposition: form-data; name=\"metadata\"\r\n\r\n"
+                        + "" + "\r\n";
+
+                String fileHeader1 = "--" + boundary + "\r\n"
+                        + "Content-Disposition: form-data; name=\"uploadedfile\"; filename=\""
+                        + fileName + "\"\r\n"
+                        + "Content-Type: application/octet-stream\r\n"
+                        + "Content-Transfer-Encoding: binary\r\n";
+
+                long fileLength = file.length() + tail.length();
+                String fileHeader2 = "Content-length: " + fileLength + "\r\n";
+                String fileHeader = fileHeader1 + fileHeader2 + "\r\n";
+                String stringData = metadataPart + fileHeader;
+
+                long requestLength = stringData.length() + fileLength;
+                connection.setRequestProperty("Content-length", "" + requestLength);
+                connection.setFixedLengthStreamingMode((int) requestLength);
+                connection.connect();
+
+                DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+                out.writeBytes(stringData);
+                out.flush();
+
+                allprogress = 0;
+                int bytesRead = 0;
+                byte buf[] = new byte[1024];
+                BufferedInputStream bufInput = new BufferedInputStream(new FileInputStream(file));
+                while ((bytesRead = bufInput.read(buf)) != -1) {
+                    // write output
+                    out.write(buf, 0, bytesRead);
+                    out.flush();
+                    allprogress += bytesRead;
+                    // update progress bar
+                    publishProgress(allprogress);
+                }
+
+                // Write closing boundary and close stream
+                out.writeBytes(tail);
+                out.flush();
+                out.close();
+
+                // Get server response
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line = "";
+                StringBuilder builder = new StringBuilder();
+                while((line = reader.readLine()) != null) {
+                    builder.append(line);
+                }
+
+            } catch (Exception e) {
+                // Exception
+            } finally {
+                if (connection != null) connection.disconnect();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            numberProgressBar.setProgress((int)((progress[0] * 100) / file.length()));
+        }
+
+        @Override
+        protected void onPostExecute(Void v) {
+            Intent intent = new Intent(LoadingActivity.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            cancel(true);
+            dialog.dismiss();
+        }
     }
 }
