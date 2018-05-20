1a1444b4cbbdd785fd8713368df56eaa05b95840
c密碼 <h35765469@yahoo.com.tw>
2018-05-20 19:20:56 +0800

    刪除多餘的import

--- app/src/main/java/com/example/john/mimicvideo/adapter/VideoSampleAdapter.java
+++ app/src/main/java/com/example/john/mimicvideo/adapter/VideoSampleAdapter.java
@@ -1,43 +1,18 @@
 package com.example.john.mimicvideo.adapter;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.example.john.mimicvideo.CameraActivity;
 import com.example.john.mimicvideo.LoadingActivity;
 import com.example.john.mimicvideo.R;
-import com.example.john.mimicvideo.ShowVideoContentActivity;
-import com.example.john.mimicvideo.TestVideoActivity;
-import com.example.john.mimicvideo.api.Api;
 import com.example.john.mimicvideo.model.VideoSample;
-import com.google.android.exoplayer2.DefaultLoadControl;
-import com.google.android.exoplayer2.ExoPlayerFactory;
-import com.google.android.exoplayer2.LoadControl;
-import com.google.android.exoplayer2.SimpleExoPlayer;
-import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
-import com.google.android.exoplayer2.extractor.ExtractorsFactory;
-import com.google.android.exoplayer2.source.ExtractorMediaSource;
-import com.google.android.exoplayer2.source.MediaSource;
-import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
-import com.google.android.exoplayer2.trackselection.TrackSelection;
-import com.google.android.exoplayer2.trackselection.TrackSelector;
-import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
-import com.google.android.exoplayer2.upstream.BandwidthMeter;
-import com.google.android.exoplayer2.upstream.DataSource;
-import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
-import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
-import com.google.android.exoplayer2.util.Util;
 
 import java.util.List;
 
