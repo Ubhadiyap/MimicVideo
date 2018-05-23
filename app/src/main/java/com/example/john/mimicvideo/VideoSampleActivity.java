c21f6df485b1f5029be4cecb179f52bc13f5bf1c
c密碼 <h35765469@yahoo.com.tw>
2018-05-24 06:52:50 +0800

    更改相機位置到斷點相機的activity

--- app/src/main/java/com/example/john/mimicvideo/VideoSampleActivity.java
+++ app/src/main/java/com/example/john/mimicvideo/VideoSampleActivity.java
@@ -3,21 +3,16 @@ package com.example.john.mimicvideo;
 import android.content.Intent;
 import android.media.MediaMetadataRetriever;
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.widget.AbsListView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.john.mimicvideo.adapter.VideoSampleAdapter;
 import com.example.john.mimicvideo.api.Api;
@@ -30,11 +25,8 @@ import com.example.john.mimicvideo.utils.SearchItemDecoration;
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class VideoSampleActivity extends BaseActivity {
@@ -127,7 +119,7 @@ public class VideoSampleActivity extends BaseActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
-                intent.setClass(VideoSampleActivity.this, FantasticCameraActivity.class);
+                intent.setClass(VideoSampleActivity.this, ContinueCameraActivity.class);
                 startActivity(intent);
             }
         });
